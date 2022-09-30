337700fca403d04c7dcb4ec26e88b8c95bd9d350
Samia <samiaab1990@gmail.com>
2022-09-29 22:26:34 -0700

    successfully installed package

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup
-from getnycholidayspy import __version__
 
 setup(
     name='getnycholidayspy',
-    version=__version__,
+    version='0.0.1',
     author='Samia B',
     author_email='samiaab1990@gmail.com',
     license='MIT',
-    packages=['getnycholidayspy'],
+    packages=['getNYCholidayspy'],
     scripts=['scripts/getnycholidayspy_test.py'],
     url='https://github.com/samiaab1990/getNYCholidayspy',
     description="A Python version of the getNYCholidays package. Retrieves a vector of official NYC holiday dates from the NYC Office of Payroll Administration's List of Holidays PDF. Allows to retrieve dates by filtering on holiday name, weekday and date.",
